fa5c71be5046ce64c7cb16979b0d5938fd700abc
fydon <112878144+Fnord007@users.noreply.github.com>
2025-01-28 23:30:19 +0100

    uuuuuuuuuuuuuuuuuuhhhhhh yea

---
+++ maxCurrentAbilities.js
@@ -0,0 +1,21 @@
+/**
+* @license StewartPrivateLicense-2.0.1
+* Copyright (c) Aerell McKnight 2023
+*
+* You may not reproduce or distribute any code inside this file without the licenser's permission.
+* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+* You may not under any circumstance republish any code from this file as your own.
+* 
+* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
+* https://github.com/Minesraft2/Blooket-Cheats/blob/main/LICENSE
+*/
+
+/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
+
+(async () => {
+    const { stateNode } = Object.values(document.querySelector('body div[id] > div > div'))[1].children[0]._owner;
+    for (const [ability, level] of Object.entries(stateNode.state.abilities)) for (let i = 0; i < (10 - level); i++) stateNode.state.game.scene.game.events.emit("level up", ability, stateNode.state.abilities[ability]++);
+    stateNode.setState({
+        level: stateNode.state.game.scene.level = [1, 3, 5, 10, 15, 25, 35].sort((a, b) => Math.abs(a - stateNode.state.level) - Math.abs(b - stateNode.state.level))[0] - 1
+    });
+})();
